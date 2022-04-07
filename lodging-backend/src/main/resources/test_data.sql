88ffc44d14b6e59c5b7b793df76cf1b0155293e1
Maciej Bartczak <maciekbartczak17@gmail.com>
2022-04-07 10:35:50 +0200

    update test_data.sql

--- lodging-backend/src/main/resources/test_data.sql
+++ lodging-backend/src/main/resources/test_data.sql
@@ -1,4 +1,6 @@
+insert into role (authority) values ('USER');
 insert into users values (1, 'test', 'test', '$2a$12$aM6aqvT1BWv0gkojKuxVMOhdqyQsn69GImWX2kYYQqw7AxdJj.0IO', 'testuser');
+insert into user_roles (user_id, role_id) values (1, 1);
 
 insert into hotel (max_guests, name, price_per_night, image_name, user_id) values (2, 'hotel 2', 400, 'test.png', 1);
 insert into hotel (max_guests, name, price_per_night, image_name, user_id) values (3, 'hotel 3', 350, 'test.png', 1);
@@ -25,3 +27,4 @@ insert into address values (9, 'Warsaw', 'Poland', 'test street');
 insert into address values (10, 'Cracow', 'Poland', 'test street');
 insert into address values (11, 'Lodz', 'Poland', 'test street');
 insert into address values (12, 'Wroclaw', 'Poland', 'test street');
+
